06f02a3b5be096e937e8ea4c956b97619b9dbea7
rodrigodpl <rodrigodpl97@gmail.com>
2018-10-12 15:34:17 +0200

    Math/random reworked

--- Kuroko Engine/Random.cpp
+++ Kuroko Engine/Random.cpp
@@ -8,20 +8,14 @@ int randomFromTo(int n1, int n2) {
 	if (n1 >= n2)
 		return 0;
 
-	// Make new seed every call, so it is as random as it gets
-	pcg32_random_t rng;
-	randomizeSeed(&rng);
 	// Calculate random number
 	int bound = n2 - n1;
-	int random_number = pcg32_boundedrand_r(&rng, bound);
+	int random_number = pcg32_boundedrand( bound);
 	return n1 + random_number;
 }
 
 float randomFloat(){
-	// Make new seed every call, so it is as random as it gets
-	pcg32_random_t rng;
-	randomizeSeed(&rng);
-	return ldexp(pcg32_random_r(&rng), -32);
+	return ldexp(pcg32_random(), -32);
 }
 
 void randomizeSeed(pcg32_random_t* seed) {
@@ -30,5 +24,5 @@ void randomizeSeed(pcg32_random_t* seed) {
 	std::default_random_engine generator(rd());
 	std::uniform_int_distribution<uint16_t> distribution(0, 0xFFFFFFFFFFFFFFFF);
 	//Randomize seed
-	pcg32_srandom_r(seed, distribution(generator), distribution(generator));
+	pcg32_srandom(distribution(generator), distribution(generator));
 }
