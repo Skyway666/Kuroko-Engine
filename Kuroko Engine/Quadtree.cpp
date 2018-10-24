69bdefffbb33850cada303ce81ae9b9c53263707
Skyway666 <lucasgarciamateu@gmail.com>
2018-10-24 19:26:34 +0200

    Setup structure for building the quadtree

---
+++ Kuroko Engine/Quadtree.cpp
@@ -0,0 +1,69 @@
+#include "Quadtree.h"
+
+
+// Quadtree
+template<class PRIMITIVE>
+Quadtree<PRIMITIVE>::Quadtree() {
+}
+
+
+template<typename PRIMITIVE>
+Quadtree<PRIMITIVE>::Quadtree(AABB limits) {
+}
+
+template<typename PRIMITIVE>
+Quadtree<PRIMITIVE>::Quadtree(std::list<GameObject*> objects) {
+}
+
+template<class PRIMITIVE>
+Quadtree<PRIMITIVE>::~Quadtree() {
+}
+
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::Create(AABB limits) {
+}
+
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::Create(std::list<GameObject*> objects) {
+}
+
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::Insert(GameObject * object) {
+}
+
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::Fill(std::list<GameObject*> objects) {
+	for (auto it = objects.begin(); it != objects.end(); it++)
+		Insert(*it);
+}
+
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::Intersect(std::list<GameObject*> objects, PRIMITIVE primitive) {
+
+}
+
+
+// Quadtree node
+template<class PRIMITIVE>
+QuadTreeNode<PRIMITIVE>::QuadTreeNode(AABB limits) {
+
+}
+
+
+template<class PRIMITIVE>
+QuadTreeNode<PRIMITIVE>::QuadTreeNode(AABB limits, std::list<GameObject*> objects) {
+
+}
+
+template<class PRIMITIVE>
+QuadTreeNode<PRIMITIVE>::CollectIntersections(std::list<GameObject*> objects, PRIMITIVE primitive) {
+
+}
+
+
+template<class PRIMITIVE>
+QuadTreeNode<PRIMITIVE>::Split() {
+
+}
+
+
---
+++ Kuroko Engine/Quadtree.h
@@ -0,0 +1,38 @@
+#ifndef QUAD_TREE
+#define QUAD_TREE
+#include <list>
+#include "Math.h"
+
+
+class GameObject;
+template<class PRIMITIVE>
+struct QuadTreeNode {
+
+	QuadTreeNode(AABB limits); // Create nodes with no objects
+	QuadTreeNode(AABB limits, std::list<GameObject*> objects); // Create nodes with objects
+
+	void CollectIntersections(std::list<GameObject*> found_obj, PRIMITIVE primitive);
+	void Split(); // creates childs and passes them the objects contained on this one
+	QuadTreeNode* childs[4];
+	AABB box;
+	bool is_leaf = true;
+	std::list<GameObject*> objects;
+};
+
+template<class PRIMITIVE>
+class Quadtree {
+public:
+	Quadtree(AABB limits); 	
+	Quadtree(std::list<GameObject*> objects);	  // Adaptive
+	~Quadtree();
+	void Create(AABB limits); 	
+	void Create(std::list<GameObject*> objects)	  // Adaptive
+	void Insert(GameObject* object);			  // Insert an object into the quadtree so it blends to fit it
+	void Fill(std::list<GameObject*> objects);    //Multiple "Inserts" at once
+	void Intersect(std::list<GameObject*> objects, PRIMITIVE primitive); 	// objects: List to be filled
+	QuadTreeNode* root; // Root of the quadtree
+	int max_splits; 	// How many splits allowed
+	int bucket_size;	// How much items can be held in a QuadTreeNode
+};
+
+#endif 
