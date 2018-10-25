5abb706e1c768be1d044c042762be9499faafd83
Skyway666 <lucasgarciamateu@gmail.com>
2018-10-25 20:24:43 +0200

    Implemented Split() and AddObject() in QuadTreeNode

--- Kuroko Engine/Quadtree.cpp
+++ Kuroko Engine/Quadtree.cpp
@@ -1,4 +1,5 @@
 #include "Quadtree.h"
+#include "GameObject.h"
 
 
 // Quadtree
@@ -27,8 +28,8 @@ void Quadtree<PRIMITIVE>::Create(std::list<GameObject*> objects) {
 }
 
 template<class PRIMITIVE>
-void Quadtree<PRIMITIVE>::Insert(GameObject * object) {
-
+bool Quadtree<PRIMITIVE>::Insert(GameObject* object) {
+	return root->AddObject(object);
 }
 
 template<class PRIMITIVE>
@@ -42,14 +43,20 @@ void Quadtree<PRIMITIVE>::Intersect(std::list<GameObject*>& objects, PRIMITIVE p
 
 }
 
+template<class PRIMITIVE>
+void Quadtree<PRIMITIVE>::DebugDraw() {
+
+}
+
 // Quadtree node
 template<class PRIMITIVE>
 QuadTreeNode<PRIMITIVE>::QuadTreeNode(AABB limits) {
 	box = limits;
+	is_leaf = true;
 }
 
 
-template<class PRIMITIVE>
+template<class PRIMITIVE> // Constructor to create childs
 QuadTreeNode<PRIMITIVE>::QuadTreeNode(AABB limits, std::list<GameObject*> objects) {
 
 
@@ -65,4 +72,66 @@ void QuadTreeNode<PRIMITIVE>::CollectIntersections(std::list<GameObject*>& objec
 template<class PRIMITIVE>
 void QuadTreeNode<PRIMITIVE>::Split() {
 
+	// Top left
+	float quarter_x = box.HalfSize().x / 2;
+	float quarter_y = box.HalfSize().z / 2;
+	float3 centre = box.CenterPoint();
+
+	
+	AABB split_box;
+	float3 new_centre;
+
+	// Top left
+	new_centre = box.Centroid + float3(-quarter_x, 0, quarter_y);
+	split_box.SetFromCenterAndSize(new_centre, box.HalfSize());
+	childs[0] = new QuadTreeNode<PRIMITIVE>(split_box);
+
+	// Top right
+	new_centre = box.Centroid + float3(quarter_x, 0, quarter_y);
+	split_box.SetFromCenterAndSize(new_centre, box.HalfSize());
+	childs[1] = new QuadTreeNode<PRIMITIVE>(split_box);
+
+	//Bottom left
+	new_centre = box.Centroid + float3(-quarter_x, 0, -quarter_y);
+	split_box.SetFromCenterAndSize(new_centre, box.HalfSize());
+	childs[2] = new QuadTreeNode<PRIMITIVE>(split_box);
+
+	//Bottom right
+	new_centre = box.Centroid + float3(quarter_x, 0, -quarter_y);
+	split_box.SetFromCenterAndSize(new_centre, box.HalfSize());
+	childs[3] = new QuadTreeNode<PRIMITIVE>(split_box);
+}
+
+template<class PRIMITIVE>
+bool QuadTreeNode<PRIMITIVE>::AddObject(GameObject * obj, int bucket_size) {
+
+	// If the gameobject is not in this node we do nothing
+	if (!box.Contains(obj->getCentroid()))
+		return false;
+
+	if (objects.size() < bucket_size)
+		objects.push_back(obj);				// If the bucket size accepts another object we add it
+	else {
+		is_leaf = false						
+		Split();							// If not we split the node, which makes it not be a leaf anymore, 
+
+		for (auto it = objects.begin(); it != objects.end(); it++) 
+			for (int i = 0; i < 4; i++)
+				childs[i]->AddObject(*it, bucket_size);				// We first fill the childs with the objects that this node contained
+		objects.clear();											// And clean the list
+		
+		for (int i = 0; i < 4; i++)									// We fill childs with the original object we wanted to add.
+			childs[i]->AddObject(obj, bucket_size);					// If not possible, the process will repeat again until it is.
+		
+		// TODO 1: Make that when the maximum number of splits is reach, the bucket size stop mattering
+
+		// TODO 2: This function could be optimized ignoring the nodes which can contain the gameobject before calling the function
+	}
+	return true;
+
+}
+
+template<class PRIMITIVE>
+int QuadTreeNode<PRIMITIVE>::getNumObj() {
+	return objects.size();
 }
--- Kuroko Engine/Quadtree.h
+++ Kuroko Engine/Quadtree.h
@@ -10,9 +10,11 @@ struct QuadTreeNode {
 	QuadTreeNode(AABB limits); // Create nodes with no objects
 	QuadTreeNode(AABB limits, std::list<GameObject*> objects); // Create nodes with objects
 
-	void CollectIntersections(std::list<GameObject*>& found_obj, const PRIMITIVE& primitive);
+	void CollectIntersections(std::list<GameObject*>& found_obj, const PRIMITIVE& primitive); // See what intersections the nodes have
 	void Split(); // creates childs and passes them the objects contained on this one
-	QuadTreeNode* childs[4];
+	bool AddObject(GameObject* obj, int bucket_size);
+	int getNumObj();
+	QuadTreeNode<PRIMITIVE>* childs[4];
 	AABB box;
 	bool is_leaf = true;
 	std::list<GameObject*> objects;
@@ -28,11 +30,12 @@ public:
 	~Quadtree();
 	void Create(AABB limits); 	
 	void Create(std::list<GameObject*> objects);	  // Adaptive(not for now)
-	void Insert(GameObject* object);			  // Insert an object into the quadtree so it blends to fit it
+	bool Insert(GameObject* object);			  // Insert an object into the quadtree so it blends to fit it
 	void Fill(std::list<GameObject*> objects);    //Multiple "Inserts" at once
 	void Intersect(std::list<GameObject*>& objects, PRIMITIVE primitive); 	// objects: List to be filled
-	QuadTreeNode* root; // Root of the quadtree
-	int max_splits; 	// How many splits allowed
+	void DebugDraw();
+	QuadTreeNode<PRIMITIVE>* root; // Root of the quadtree
+	int max_splits; 	// How many splits allowed (not used for now)
 	int bucket_size;	// How much items can be held in a QuadTreeNode
 };
 
